63b249abd91140061a5641823203d471331a0fff
thehyperhydra <74665127+thehyperhydra@users.noreply.github.com>
2021-06-09 20:06:06 +0530

    Add files via upload

---
+++ js/Code.js
@@ -0,0 +1,27 @@
+const accessCode1 = "VARIABLE";
+const accessCode2 = "FUNCTION";
+const accessCode3 = "DATABASE";
+
+function clues() {
+    
+    fill("white")
+    textSize(15)
+    text("R E V B A I L A", 100,50)
+    fill("lightblue")
+    text("Hint: Always changing, not constant !!", 100,70)
+// add code for displaying rest of the names and the hints.
+
+fill("white")
+    textSize(15)
+    text("U T O F N C I N   ", 700,150)
+    fill("lightblue")
+    text("Hint: Performs a task !!", 700,170)
+
+    fill("white")
+    textSize(15)
+    text("D T B A S A E A", 100,250)
+    fill("lightblue")
+    text("Hint: stores info !!", 100,270)
+    
+
+}
